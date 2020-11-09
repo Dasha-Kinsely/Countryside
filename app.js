0378ea076b60b277fe4cc649281fd2f453119ed8
Dasha-Kinsely <dashakinsely@gmail.com>
2020-11-09 12:35:10 -0800

    Initialize

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const expressValidator = require('express-validator');
+
+require('dotenv').config();
